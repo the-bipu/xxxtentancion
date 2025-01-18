f52eba5c6324c91b6330839b7c6d07fa99565da2
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-01-18 11:16:44 +0530

    update homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,16 +7,34 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+import logo from "/logo.svg";
+import logoM from "/logoM.svg";
+
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center bg-tentan">
-      <div className="flex flex-col items-center gap-16">
+    <div className="flex h-full w-full flex-col">
 
-        <header className="flex flex-col items-center gap-9">
-          changes
+      <div className="min-h-screen w-full flex flex-col items-center justify-center gap-16 bg-tentan">
+        <header className="flex flex-col w-11/12 justify-center gap-9">
+          <img src={logoM} alt="" />
         </header>
-
       </div>
+
+      <div className="min-h-screen flex md:flex-row flex-col w-full justify-center items-center">
+        <div className="md:w-1/4 w-full md:h-screen h-52 flex flex-col items-center justify-center bgHoverCard">
+          <img src={logo} alt="" className="w-11/12" />
+        </div>
+        <div className="md:w-1/4 w-full md:h-screen h-52 flex flex-col items-center justify-center bgHoverCard">
+          <img src={logo} alt="" className="w-11/12" />
+        </div>
+        <div className="md:w-1/4 w-full md:h-screen h-52 flex flex-col items-center justify-center bgHoverCard">
+          <img src={logo} alt="" className="w-11/12" />
+        </div>
+        <div className="md:w-1/4 w-full md:h-screen h-52 flex flex-col items-center justify-center bgHoverCard">
+          <img src={logo} alt="" className="w-11/12" />
+        </div>
+      </div>
+
     </div>
   );
 }
