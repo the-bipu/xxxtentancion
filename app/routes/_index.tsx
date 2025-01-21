5d47ff43f9d3bcc53b6cce944cff8547c4a39c72
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-01-21 23:49:43 +0530

    update homepage
    added new font, added new pictures and update the homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -67,15 +67,23 @@ export default function Index() {
 
       <div className="flex md:flex-row flex-col min-h-screen bg-black">
         <div className="md:w-1/4 w-full md:h-screen h-auto">
-          <div className="w-full md:h-2/6 h-64 bgHoverSecond">hello</div>
-          <div className="w-full md:h-4/6 h-64 bgHoverFour">hello</div>
+          <div className="flex items-center justify-center relative w-full md:h-2/6 h-64 bgHoverFive">
+            <span className="text-2xl font-semibold absolute top-10">Jahseh</span>
+          </div>
+          <div className="flex items-center justify-center relative w-full md:h-4/6 h-64 bgHoverThird">
+            <span className="text-2xl font-semibold absolute bottom-10">Jahseh</span>
+          </div>
         </div>
         <div className="flex md:w-1/4 w-full md:h-screen min-h-96 relative items-center justify-center bgHoverFirst">
           <span className="text-2xl font-semibold absolute top-10">Young Jahseh</span>
         </div>
         <div className="md:w-1/4 w-full md:h-screen h-auto">
-          <div className="w-full md:h-4/6 h-64 bgHoverFour">hello</div>
-          <div className="w-full md:h-2/6 h-64 bgHoverSecond">hello</div>
+          <div className="flex items-center justify-center relative w-full md:h-4/6 h-64 bgHoverFour">
+            <span className="text-2xl font-semibold absolute top-10">Jahseh</span>
+          </div>
+          <div className="flex items-center justify-center relative w-full md:h-2/6 h-64 bgHoverSecond">
+            <span className="text-2xl font-semibold absolute bottom-10">Jahseh</span>
+          </div>
         </div>
         <div className="md:flex hidden md:w-1/4 w-full h-screen relative items-center justify-center bgHoverKid">
           <span className="text-2xl font-semibold absolute top-10">Kid Jahseh</span>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ export default {
           "Segoe UI Symbol",
           "Noto Color Emoji",
         ],
+        receipt: ['Fake Receipt', 'sans-serif'],
       },
     },
   },
