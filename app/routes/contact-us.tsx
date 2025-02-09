9c0249247c467e2a385599914af726c9670e5229
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-02-09 23:49:42 +0530

    Update contact-us.tsx

--- app/routes/contact-us.tsx
+++ app/routes/contact-us.tsx
@@ -22,9 +22,14 @@ const ContactUs = () => {
                 <img src={kid} alt="" className="w-72" />
 
                 <div className="font-bold text-xl">Reach Us,</div>
-                <div className="flex flex-row gap-3">
-                    <input type="text" className="bg-white border border-gray-500 border-dotted rounded indent-2 py-2" placeholder="Your Name" />
-                    <input type="text" className="bg-white border border-gray-500 border-dotted rounded indent-2 py-2" placeholder="Your Email" />
+                <div className="flex flex-col gap-4">
+                    <div className="flex flex-row gap-3">
+                        <input type="text" className="bg-white border border-gray-500 border-dotted rounded indent-2 py-2" placeholder="Your Name" />
+                        <input type="text" className="bg-white border border-gray-500 border-dotted rounded indent-2 py-2" placeholder="Your Email" />
+                    </div>
+                    <div>
+                        <button>Send</button>
+                    </div>
                 </div>
             </div>
 
